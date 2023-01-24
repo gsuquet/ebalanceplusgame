def11318b83fdd20b81089de9cd72b46c9a356f7
Gabriel Suquet <gabriel.suquet@student.junia.com>
2023-01-24 11:19:26 +0100

    [EB-4] Add Alert Component

---
+++ src/components/Alert.vue
@@ -0,0 +1,35 @@
+<template>
+    <div :class="alertClass+' alert'" v-if="display">
+        <span class="closebtn" @click="closeAlert">&times;</span>
+        <p>{{ alertText }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        props: {
+            alertClass: String,
+            alertText: String,
+            shouldDisplay: Boolean
+        },
+        data() {
+            return {
+                display: this.shouldDisplay,
+            }
+        },
+        watch: {
+            shouldDisplay: function (newVal) {
+                this.display = newVal;
+            }
+        },
+        methods: {
+            closeAlert() {
+                this.display = false;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+@import '../styles/components/alert.scss'
+</style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import Alert from '../components/Alert.vue';
 import Board from '../components/Board.vue';
+import { useConsumptionStore } from '../stores/ConsumptionStore';
+const consumptionStore = useConsumptionStore();
+const { t } = useI18n()
 </script>
 
 <template>
+    <Alert :should-display="consumptionStore.isOverConsumption" 
+        alert-class="danger-alert" 
+        :alert-text="t('alert.overConsumption')"/>
     <Board />
 </template>
 
