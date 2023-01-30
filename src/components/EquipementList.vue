f49ce89e7039af1b63124a2418dbefe5019f9677
gabrieljunia <91719818+gabrieljunia@users.noreply.github.com>
2023-01-30 11:43:43 +0100

    Feature/EB-1 (#2)
    * test commit
    
    * test commit
    
    * commit file for the equipment list with the store
    
    * commit the list + popup
    
    * Commit popup for add consumption working
    
    * commit the pop up for adding the consumption
    
    * [ED-1] Add equipment list to game
    
    ---------
    
    Co-authored-by: Antoine NEUVILLE <antoine.neuville@student.junia.com>

---
+++ src/components/EquipementList.vue
@@ -0,0 +1,78 @@
+<script lang="ts">
+    import { useEquipmentStore} from "../stores/EquipmentStore";
+    //import EquipmentType from "../components/EquipementType.vue";
+    import WashingMachine from "../icons/WashingMachine.vue";
+    import Dishwasher from "../icons/Dishwasher.vue";
+    import ArrowRight from "../icons/ArrowRight.vue";
+    import Equipments from "../components/Equipments.vue";
+    
+    export default {
+        setup() {
+            const store = useEquipmentStore();
+            store.getEquipmentData();
+
+            return {store}
+
+        }, 
+        data() {
+            return {
+                showList: false as boolean 
+            }
+        },
+        components: {
+            //EquipmentType,
+            WashingMachine, 
+            Dishwasher,
+            ArrowRight,
+            Equipments,
+        },
+        methods: {
+            expandList(showList: boolean) {
+                if(showList == false) 
+                    showList = true;
+                else 
+                    showList = false;
+                return (showList);
+            }
+        }
+    }
+    
+    let showList = false;        
+</script>
+
+<template>
+    <main>   
+        <!--        
+        <div class="menu-toggle-wrap">
+            <button class="menu-toggle">
+                <ArrowRight class="material-icons"/>
+            </button>
+        </div>
+        -->
+        <div class="list-container">
+            <div class="type-list">
+                <div class="boucle" v-for="equipment_type in store.getTypeOnly()" >  
+                    <div class="container" @click="showList = expandList(showList)">
+                        <h1>
+                            {{ equipment_type }}
+                        </h1>
+                        <!-- TODO: change that !!! -->
+                        <div class="icon" v-if="equipment_type == 'lave-vaisselle'">
+                            <Dishwasher class="material-icons"/>
+                        </div>
+                        <div class="icon" v-else >
+                            <WashingMachine class="material-icons"/>
+                        </div> 
+                    </div>
+                    <div class="equipment-container" v-if="showList" >
+                            <Equipments v-for="equipment in store.getEquipmentByType(equipment_type)" :key="equipment_type"  :equipment="equipment"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main> 
+</template>
+
+<style lang="scss">
+    @import "../styles/components/list.scss";
+</style>
