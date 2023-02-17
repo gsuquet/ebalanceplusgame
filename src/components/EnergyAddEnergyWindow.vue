a5fbb3c6f4992ce9329ecd8205828d49f2eabc9f
Gabriel Suquet <gabriel.suquet@student.junia.com>
2023-02-17 15:03:45 +0100

    [EB-55] Create new general popup to keep the codebase DRY

--- src/components/EnergyAddEnergyWindow.vue
+++ src/components/EnergyAddEnergyWindow.vue
@@ -2,76 +2,37 @@
     import { useEnergyStore } from '../stores/EnergyStore';
     import { useConsumptionStore } from '../stores/ConsumptionStore';
     import { Equipment } from '../types/Equipment';
-    import CardPopupHeader from './CardPopupHeader.vue';
-    import CardPopupContent from './CardPopupContent.vue';
-    import CardPopupAmountModifier from './CardPopupAmountModifier.vue';
-    import CardPopupTimeModifier from './CardPopupTimeModifier.vue';
-    import CardPopupSaveButtons from './CardPopupSaveButtons.vue';
+    import ConsumptionProductionPopupWindow from './ConsumptionProductionPopupWindow.vue';
 </script>
 
 <template>
-    <section class="popup-window">
-        <div class="color-banner" :style="{'background-color':equipment.type.color}"/>
-        <div class="card">
-            <CardPopupHeader 
-                :equipment-icon="equipment.type.icon_name"
-                :equipment-color="equipment.type.color"
-                :consumption-type="type"
-                @close-popup="closePopup"/>
-            <CardPopupContent 
-                :consumption-amount="amount"
-                :equipment-price="price"
-                :times="{timeStart:startHour,timeEnd:endHour}"
-                :is-cost="false"/>
-            <CardPopupAmountModifier 
-                :amount="amount"
-                :max-amount="maxAmount"
-                :min-amount="equipment.equipmentConsumptionParams.minConsumption"
-                :step-amount="equipment.equipmentConsumptionParams.step"
-                i18n-key="input.consumption"
-                @amount="(value) => updateConsumptionAmount(value)"/>
-            <CardPopupAmountModifier
-                v-if="equipment.equipmentCostParams.isCostEditable"
-                :amount="price"
-                :max-amount="equipment.equipmentCostParams.maxCost"
-                :min-amount="equipment.equipmentCostParams.minCost"
-                :step-amount="equipment.equipmentCostParams.step"
-                i18n-key="input.cost"
-                @amount="(value) => updatePrice(value)"/>
-            <CardPopupTimeModifier 
-                :start-hour="startHour"
-                :end-hour="endHour"
-                :max-duration="equipment.type.equipmentTypeDurationParams.maxDuration"
-                :min-duration="equipment.type.equipmentTypeDurationParams.minDuration"
-                :step-duration="equipment.type.equipmentTypeDurationParams.step"
-                :original-duration="equipment.type.equipmentTypeDurationParams.originalDuration"
-                :is-duration-length-editable="equipment.type.equipmentTypeDurationParams.isDurationLengthEditable"
-                :input-error="inputError"
-                @start-hour="(value) => updateStartHour(value)"
-                @end-hour="(value) => updateEndHour(value)"/>
-            <CardPopupSaveButtons
-                @save="saveConsumption"
-                @cancel="closePopup"/>
-        </div>
-    </section>
+    <ConsumptionProductionPopupWindow
+        :id="consumptionId"
+        :type="type"
+        :props-amount="amount"
+        :props-max-energy-amount="maxAmount"
+        :props-price="price"
+        :equipment="equipment"
+        :indexes="{start:startIndex,end:endIndex}"
+        :props-is-initial-add-popup="true"
+        @close-popup="closePopup"
+        @save="saveConsumption"
+        @cancel="closePopup"
+        @delete="closePopup"/>
 </template>
 
 <script lang="ts">
     export default {
         name: 'EnergyAddEnergyWindow',
         components: {
-            CardPopupHeader,
-            CardPopupAmountModifier,
-            CardPopupTimeModifier
+            ConsumptionProductionPopupWindow
         },
         data() {
             return {
                 energyStore: useEnergyStore(),
                 consumptionStore: useConsumptionStore(),
+                consumptionId: '',
                 type: this.$t("energy.storeEnergy"),
-                inputError: false as boolean,
-                startHour: '00:15' as string,
-                endHour: '00:30' as string,
                 startIndex: 0 as number,
                 endIndex: 0 as number,
                 maxAmount: 10 as number,
@@ -120,55 +81,17 @@
             closePopup() {
                 this.energyStore.clickOnStoreEnergy();
             },
-            updateConsumptionAmount(newConsumption:number) {
-                this.amount=newConsumption;
-            },
-            updatePrice(newPrice:number) {
-                this.price=newPrice;
-            },
-            updateStartHour(newStartHour:string) {
-                this.startHour=newStartHour;
-                this.setStartAndEndIndex();
-                this.updateMaxAmount();
-            },
-            updateEndHour(newEndHour:string) {
-                this.endHour=newEndHour;
-                this.setStartAndEndIndex();
-                this.updateMaxAmount();
-            },
-            setStartAndEndIndex() {
-                const indexes = this.consumptionStore.convertTimesToIndexes(this.startHour, this.endHour);
-                this.startIndex = indexes.indexStart;
-                this.endIndex = indexes.indexEnd;
-            },
-            updateMaxAmount() {
-                this.maxAmount=(this.energyStore.maxEnergy-this.energyStore.storedEnergy)/((this.endIndex-this.startIndex)+1)
-            },
-            checkAmountIsUnderMax() {
-                if(this.amount>this.maxAmount) {
-                    return false;
-                } else {
-                    return true;
-                }
-            },
-            saveConsumption() {
-                if(this.consumptionStore.checkTimeInput(this.startHour, this.endHour)) {
-                    this.inputError=false;
-                    if (this.checkAmountIsUnderMax()) {
-                        this.consumptionStore.addConsumption(this.startIndex, this.endIndex, this.equipment, this.amount, this.price);
-                        this.energyStore.clickOnStoreEnergy();
-                    }
-                } else {
-                    this.inputError=true;
-                }
+            saveConsumption(save:{startIndex:number, endIndex:number,amount:number,price:number}) {
+                this.consumptionStore.addConsumption(save.startIndex, save.endIndex, this.equipment, save.amount, save.price);
+                this.energyStore.clickOnStoreEnergy();
             },
             getRandomEquipmentIdString() {
                 return Math.random().toString(36).substr(2, 9);
             }
         },
         mounted() {
-            this.updateMaxAmount();
             this.equipment.id=this.getRandomEquipmentIdString();
+            this.consumptionId=this.getRandomEquipmentIdString();
         }
     }
 </script>
