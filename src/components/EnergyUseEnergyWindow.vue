b83e530d14a227f05be47769327a45b785b34024
Gabriel Suquet <gabriel.suquet@student.junia.com>
2023-02-17 15:03:45 +0100

    [EB-55] Create new general popup to keep the codebase DRY

---
+++ src/components/EnergyUseEnergyWindow.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import ConsumptionProductionPopupWindow from './ConsumptionProductionPopupWindow.vue';
+</script>
+
+<template>
+    <ConsumptionProductionPopupWindow
+        :id="''"
+        :equipment="null"
+        :type="''"
+        :amount="0"
+        :max-energy-amount="0"
+        :price="0"
+        :indexes="{start:0,end:0}"
+        @close-popup=""
+        @save=""
+        @cancel=""
+        @delete=""/>
+</template>
+
+<script lang="ts">
+    export default
+</script>
