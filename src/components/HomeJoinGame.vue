ec9f37ce49a0c8b61aeffb0df862cb5a2dcf8cc3
Gabriel Suquet <gabriel.suquet@student.junia.com>
2023-03-02 10:42:39 +0100

    [EB-16] Add components for join game component

---
+++ src/components/HomeJoinGame.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="home-join-game section">
+        <div class="section-title">
+            <h1>{{ $t("home.joinGame") }}</h1>
+        </div>
+        <p class="description">
+            {{ $t("home.joinGameDescription") }}
+        </p>
+        <div class="btn-container">
+            <button class="btn join-game-btn" @click="joinGame">
+                <Router-Link to="/setup">
+                    {{ $t("button.join") }}
+                </Router-Link>
+            </button>
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "HomeJoinGame",
+        data() {
+            return {
+                gameParametersStore: useGameParametersStore()
+            };
+        },
+        methods: {
+            joinGame() {
+                console.debug("Joining game");
+            }
+        }
+    }
+</script>
