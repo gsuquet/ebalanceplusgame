f3b1a682a796821fda1f5aa3cf7f85de4723dd34
Gabriel Suquet <gabriel.suquet@student.junia.com>
2023-03-03 10:45:08 +0100

    [EB-16] Refractor Scenario & production objects

--- src/stores/ScenarioStore.ts
+++ src/stores/ScenarioStore.ts
@@ -1,98 +1,36 @@
 import { defineStore } from 'pinia';
-import { Scenario, ScenarioLocale} from '../types/Scenario';
-import { EquipmentType, EquipmentTypeLocale } from '../types/EquipmentType';
-import { errorScenario, errorScenarioLocale } from '../assets/entityErrorScenario';
-import { convertI18nObjectToLocale } from '../helpers/translation';
+import { Scenario } from '../types/Scenario';
+import { errorScenario } from '../assets/entityErrorScenario';
+import { EquipmentType } from '../types/EquipmentType';
 
 
 export const useScenarioStore = defineStore({ id: "ScenarioStore", 
     state: () => {
         return {
             scenarios: [errorScenario] as Scenario[],
-            scenariosLocale: [errorScenarioLocale] as ScenarioLocale[],
-            clickedScenario: null as null | ScenarioLocale
+            clickedScenario: null as null | Scenario
         };
     },
 
     actions: {
-        async getAllScenario() {
+        async fetchAllScenarios() {
             const data = (await import ('../data/scenario.json')).default;
             this.scenarios = data as Scenario[];
-            this.convertScenarioListToScenarioLocaleList();
-            this.clickedScenario = this.scenariosLocale[0];
+            this.clickedScenario = this.scenarios[0];
         },
-        setClickedScenario(scenario: ScenarioLocale | null){
+        setClickedScenario(scenario: Scenario | null){
             this.clickedScenario = scenario;
         },
-        getListOfEquipmentTypeLocale(listEquipmentTypes: EquipmentType[]) {
-            const listEquipmentTypesLocales: EquipmentTypeLocale[] = []
-            const locale = useGameParametersStore().language;
-            for(const equipmentType of listEquipmentTypes ) {
-                listEquipmentTypesLocales.push({
-                    id: equipmentType.id,
-                    name: convertI18nObjectToLocale(equipmentType.names, locale),
-                    icon_name: equipmentType.icon_name,
-                    color: equipmentType.color,
-                    isBattery: equipmentType.isBattery,
-                    isCharging: equipmentType.isCharging,
-                    equipmentTypeDurationParams: equipmentType.equipmentTypeDurationParams
-                } as EquipmentTypeLocale); 
-            }
-            return listEquipmentTypesLocales;
-        },
-        //Not sure if it is really best practice
-        convertNameToNameLocale(scenario: Scenario) {
-            const locale = useGameParametersStore().language;
-            return convertI18nObjectToLocale(scenario.names, locale);
-        },
-        convertDayToDayLocale(scenario: Scenario) {
-            const locale = useGameParametersStore().language;
-            return convertI18nObjectToLocale(scenario.days, locale);
-        },
-        convertDescriptionToDescriptionLocale(scenario: Scenario){
-            const locale = useGameParametersStore().language;
-            return convertI18nObjectToLocale(scenario.descriptions, locale);
-        },
-        convertScenarioToScenarioLocale(scenario: Scenario) {
-            const listEquipmentLocale: EquipmentTypeLocale[] = this.getListOfEquipmentTypeLocale(scenario.equipment_types); 
-            const name: string = this.convertNameToNameLocale(scenario);
-            const day: string = this.convertDayToDayLocale(scenario);
-            const description: string = this.convertDescriptionToDescriptionLocale(scenario);
-            const scenarioLocale: ScenarioLocale = {id: scenario.id,
-                                                    name: name, 
-                                                    day: day, 
-                                                    season: scenario.season, 
-                                                    icon: scenario.icon, 
-                                                    color: scenario.color,
-                                                    description: description,
-                                                    equipment_type_local: listEquipmentLocale,
-                                                    initial_consumption: scenario.initial_consumption,
-                                                    energyStorageParameters: scenario.energyStorageParameters,
-                                                    energyMarketParameters: scenario.energyMarketParameters,
-                                                    moneyParameters: scenario.moneyParameters
-                                                }; 
-            return scenarioLocale;
-        },
-        convertScenarioListToScenarioLocaleList(){
-            const listScenarioLocale: ScenarioLocale[] = [];
-            for(const scenario of this.scenarios) {
-                listScenarioLocale.push(this.convertScenarioToScenarioLocale(scenario));
-            }
-            this.scenariosLocale = listScenarioLocale;
-        },
-
-        
-        //TODO: Add the internationalisation handler 
-    }, 
+    },
     getters: {
         getScenarioById:(state)=>(id: string) => {
             return state.scenarios.find(scenario => scenario.id === id);
-        }, getEquipmentBySeason:(state)=>(season: string) => {
-            return state.scenarios.find(scenario => scenario.season === season);
+        }, getEquipmentBySeasonId:(state)=>(seasonId: string) => {
+            return state.scenarios.find(scenario => scenario.season.id === seasonId);
         },
-        getEquipmentTypeFromScenario:(state)=>(scenarioLocale : ScenarioLocale) => {
-            const equipmentByTypes : EquipmentTypeLocale[] = [];
-            for(const equipment of scenarioLocale.equipment_type_local){
+        getEquipmentTypeFromScenario:(state)=>(scenario : Scenario) => {
+            const equipmentByTypes : EquipmentType[] = [];
+            for(const equipment of scenario.equipment_types){
                 if(!equipmentByTypes.includes(equipment)){
                     equipmentByTypes.push(equipment);
                 }
@@ -100,7 +38,7 @@ export const useScenarioStore = defineStore({ id: "ScenarioStore",
             return equipmentByTypes;
         },
         getClickedScenario: state => () => state.clickedScenario,
-        getRandomLocaleScenario: state => () => state.scenariosLocale[Math.floor(Math.random() * state.scenariosLocale.length)],
+        getRandomScenario: state => () => state.scenarios[Math.floor(Math.random() * state.scenarios.length)],
         getInitialConsumptionCopy: state => () => {
             if(state.clickedScenario){
                 return JSON.parse(JSON.stringify(state.clickedScenario.initial_consumption));
